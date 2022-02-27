81c6062a5b448c4cd12c5acf6b91a2611f9fb156
Martin Rünk <knyrr@users.noreply.github.com>
2022-02-27 21:41:11 +0200

    kodune töö

--- clock.js
+++ clock.js
@@ -10,13 +10,16 @@ let year = d.getFullYear();
 
 let fontSize = 10;
 
-//let bColor = "brown";
-
 let monthsEST = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli","august", "september", "oktoober", "november", "detsember"];
 let daysEST = ["pühapäev", "esmaspäev", "teisipäev", "kolmapäev", "neljapäev", "reede", "laupäev"];
 
 let monthsENG = ["January", "February", "March", "April", "May", "June", "July","August", "September", "October", "November", "December"];
-let daysENG = ["Sunday", "Mondya", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let daysENG = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+let workURLs =["images/Metsat66_1947.jpg", "images/Yrgkogukondlik_1949.jpg", "images/Suitsutare_1948.jpg", "images/M6isnikud_1947.jpg", "images/P6levkivit66stus_1947.jpg"];
+let workCaptionsEST = ["Metsatöö. 1947", "Põllumajandus ürgkogukondlikul ajastul. 1949", "Õhtu suitsutares. 1948", "Mõisnikud tõllas. 1947", "Eesti põlevkivitööstuse areng. 1947"];
+let workCaptionsENG = ["Forestry. 1947", "Agriculture in the prehistoric era. 1949", "Evening in the smokehouse. 1948", "Landlords in the chariot. 1947", "Development of the Estonian oil shale industry. 1947"];
+let work=0; 
 
 let secondsContainer = document.getElementById('seconds');
 let minutesContainer = document.getElementById('minutes');
@@ -26,99 +29,151 @@ let monthContainer = document.getElementById('month');
 let dateContainer = document.getElementById('date');
 let yearContainer = document.getElementById('year');
 
-let lang = "EST";
-
-/* let corner1 = document.getElementById('corner1');
+let container = document.getElementById('container');
+let corner1 = document.getElementById('corner1');
 let corner2 = document.getElementById('corner2');
 let corner3 = document.getElementById('corner3');
-let corner4 = document.getElementById('corner4'); */
+let corner4 = document.getElementById('corner4');
+let caption = document.getElementById('caption');
+
+let lang = "EST";
 
 dateContainer.innerHTML = date+".&nbsp;";
 monthContainer.innerHTML = monthsEST[month]+"&nbsp;";
 dayContainer.innerHTML = daysEST[day];
 yearContainer.innerHTML = year;
 
-/* secondsContainer.innerHTML = ":"+seconds;
-minutesContainer.innerHTML = ":"+minutes;
-hoursContainer.innerHTML = hours; */
 updateClock();
 
 window.setInterval(updateClock, 1000);
 
-document.getElementById('smaller').addEventListener("click", function(){
-    fontSize--;
-    document.getElementById('container').style.fontSize = fontSize + "vh";
-})
-document.getElementById('bigger').addEventListener("click", function(){
-    fontSize++;
-    document.getElementById('container').style.fontSize = fontSize + "vh";
-})
-
 document.getElementById('langENG').addEventListener("click", function(){
     lang = "ENG";
 	if (lang=="ENG"){
-    dateContainer.innerHTML = date+"&nbsp;";
-    monthContainer.innerHTML = monthsENG[month]+"&nbsp;";
-    dayContainer.innerHTML = daysENG[day];
+		dateContainer.innerHTML = date+"&nbsp;";
+		monthContainer.innerHTML = monthsENG[month]+"&nbsp;";
+		dayContainer.innerHTML = daysENG[day];
+		caption.innerHTML = workCaptionsENG[work];
 	}
 })
 document.getElementById('langEST').addEventListener("click", function(){
     lang = "EST";
 	
 	if (lang=="EST"){
-    dateContainer.innerHTML = date+".&nbsp;";
-    monthContainer.innerHTML = monthsEST[month]+"&nbsp;";
-    dayContainer.innerHTML = daysEST[day];    
+		dateContainer.innerHTML = date+".&nbsp;";
+		monthContainer.innerHTML = monthsEST[month]+"&nbsp;";
+		dayContainer.innerHTML = daysEST[day];
+		caption.innerHTML = workCaptionsEST[work];
 	}
 })
 
-document.getElementById('corner1').addEventListener("mouseover", function(){
+corner1.addEventListener("mouseover", function(){
 	document.body.style.backgroundColor = "DarkOliveGreen";
 })
 
-document.getElementById('corner2').addEventListener("mouseover", function(){
+corner2.addEventListener("mouseover", function(){
 	document.body.style.backgroundColor = "DimGrey";
 })
 
-document.getElementById('corner3').addEventListener("mouseover", function(){
+corner3.addEventListener("mouseover", function(){
 	document.body.style.backgroundColor = "Chocolate";
 })
 
-document.getElementById('corner4').addEventListener("mouseover", function(){
-	document.body.style.backgroundColor = "Khaki";
+corner4.addEventListener("mouseover", function(){
+	document.body.style.backgroundColor = "Peru";
 })
 
-document.getElementById('container').addEventListener("mouseover", function(){
+container.addEventListener("mouseover", function(){
 	document.body.style.backgroundColor = "SaddleBrown";
 })
 
-/*
-
-document.getElementById('corner1').addEventListener("click", function(){
-	//bColor = "pink";
-	document.getElementById('corner2').style.backgroundColor = "green";
+corner1.addEventListener("click", function(){
+	work=1;
+	container.style.backgroundImage = "url('"+workURLs[work]+"')";
+	if (lang=="EST"){
+		caption.innerHTML = workCaptionsEST[work];	
+	} else {
+		caption.innerHTML = workCaptionsENG[work];
+	}
 })
-document.getElementById('corner2');
-document.getElementById('corner3');
-document.getElementById('corner4'); */
+
+corner2.addEventListener("click", function(){
+	work=2;
+	container.style.backgroundImage = "url('"+workURLs[work]+"')";
+	if (lang=="EST"){
+		caption.innerHTML = workCaptionsEST[work];	
+	} else {
+		caption.innerHTML = workCaptionsENG[work];
+	}
+})
+
+corner3.addEventListener("click", function(){
+	work=3;
+	container.style.backgroundImage = "url('"+workURLs[work]+"')";
+	if (lang=="EST"){
+		caption.innerHTML = workCaptionsEST[work];	
+	} else {
+		caption.innerHTML = workCaptionsENG[work];
+	}
+})
+
+corner4.addEventListener("click", function(){
+	work=4;
+	container.style.backgroundImage = "url('"+workURLs[work]+"')";
+	if (lang=="EST"){
+		caption.innerHTML = workCaptionsEST[work];	
+	} else {
+		caption.innerHTML = workCaptionsENG[work];
+	}	
+})
+
+container.addEventListener("click", function(){
+	work=0;
+	container.style.backgroundImage = "url('"+workURLs[work]+"')";
+	if (lang=="EST"){
+		caption.innerHTML = workCaptionsEST[work];	
+	} else {
+		caption.innerHTML = workCaptionsENG[work];
+	}	
+})
+
+
 
 function updateClock(){
     d = new Date();
-    hours = d.getHours();
-    minutes = d.getMinutes();
-    seconds = d.getSeconds();
+	hours = d.getHours();
+	minutes = d.getMinutes();
+	seconds = d.getSeconds();	
+    if (lang=="EST"){
+		if(hours<10){
+			hours = "0"+hours;
+		}
+		if(minutes<10){
+			minutes = "0"+minutes;
+		}
+		if(seconds<10){
+			seconds = "0"+seconds;
+		}
 
-    if(hours<10){
-        hours = "0"+hours;
-    }
-    if(minutes<10){
-        minutes = "0"+minutes;
-    }
-    if(seconds<10){
-        seconds = "0"+seconds;
-    }
+		secondsContainer.innerHTML = ":"+seconds;
+		minutesContainer.innerHTML = ":"+minutes;
+		hoursContainer.innerHTML = hours;
+	} else {
+		let ampm = hours >= 12 ? 'pm' : 'am';
+		hours = hours % 12;
 
-    secondsContainer.innerHTML = ":"+seconds;
-    minutesContainer.innerHTML = ":"+minutes;
-    hoursContainer.innerHTML = hours;
+		if(hours<10){
+			hours = "0"+hours;
+		}
+		if(minutes<10){
+			minutes = "0"+minutes;
+		}
+		if(seconds<10){
+			seconds = "0"+seconds;
+		}
+
+		hoursContainer.innerHTML = hours;
+		minutesContainer.innerHTML = ":"+minutes;
+		secondsContainer.innerHTML = ":"+seconds+ampm;
+	}
 }
