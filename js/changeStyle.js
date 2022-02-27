0f606c1a8a35fdef01c26213f3bf8ffb5fe19434
Kristo Põlluvee <89929763+kristoplv@users.noreply.github.com>
2022-02-27 20:09:55 +0200

    new Styles

--- js/changeStyle.js
+++ js/changeStyle.js
@@ -1,6 +1,10 @@
 var clock = document.getElementById("clock");
 var blueStyle = document.getElementById("styleChange");
 var normStyle = document.getElementById("styleChange1");
+var redStyle = document.getElementById("styleChange2");
+var peppaStyle = document.getElementById("styleChange3");
+var woofStyle = document.getElementById("styleChange4");
+var catStyle = document.getElementById("styleChange5");
 
 blueStyle.onclick = function(){
     clock.style.backgroundImage = 'url("/pictures/clocky.jpg")';
@@ -8,4 +12,16 @@ blueStyle.onclick = function(){
 
 normStyle.onclick = function(){
     clock.style.backgroundImage = 'url("/pictures/clock.png")';
+}
+redStyle.onclick = function(){
+    clock.style.backgroundImage = 'url("/pictures/redC.png")';
+}
+peppaStyle.onclick = function(){
+    clock.style.backgroundImage = 'url("/pictures/peppaC.png")';
+}
+woofStyle.onclick = function(){
+    clock.style.backgroundImage = 'url("/pictures/doggoC.png")';
+}
+catStyle.onclick = function(){
+    clock.style.backgroundImage = 'url("/pictures/catC.png")';
 }
