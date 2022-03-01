236aa84842ac567011487585ff4311503718789a
Robin Kukke <robkukke@users.noreply.github.com>
2022-03-01 23:29:01 +0200

    HTML changes, CSS changes, added language changing, added time format changing

--- clock.js
+++ clock.js
@@ -2,50 +2,53 @@ let d = new Date();
 let hours = d.getHours();
 let minutes = d.getMinutes();
 let seconds = d.getSeconds();
+let day = d.getDay();
 let date = d.getDate();
 let month = d.getMonth();
 let year = d.getFullYear();
-let day = d.getDay();
 
-let fontSize = 13;
-let lang = "EST";
+let is24Hour = true;
+let amPm;
 
-if (lang == "EST") {
+let days = ["pühapäev", "esmaspäev", "teisipäev", "kolmapäev", "neljapäev", 
+"reede", "laupäev"];
+let months = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli", 
+"august", "september", "oktoober", "november", "detsember"];
 
-}
-if (lang == "ENG") {
-
-}
-
-let months = ["Jaanuar", "Veebruar", "Märts", "Aprill", "Mai", "Juuni", "Juuli", "August", "September", "Oktoober", "November", "Detsember"];
-let days = ["Pühapäev", "Esmaspäev", "Teisipäev", "Kolmapäev", "Neljapäev", "Reede", "Laupäev"];
+let daysEN = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", 
+"saturday"];
+let monthsEN = ["january", "february", "march", "april", "may", "june", "july", 
+"august", "september", "october", "november", "december"];
 
 let hoursContainer = document.getElementById('hours');
 let minutesContainer = document.getElementById('minutes');
 let secondsContainer = document.getElementById('seconds');
+let dayContainer = document.getElementById('day');
 let dateContainer = document.getElementById('date');
 let monthContainer = document.getElementById('month');
-let dayContainer = document.getElementById('day');
 
+dayContainer.innerHTML = days[day];
 dateContainer.innerHTML = date;
 monthContainer.innerHTML = months[month];
 document.getElementById('year').innerHTML = year;
-dayContainer.innerHTML = days[day];
-/*
-hoursContainer.innerHTML = hours;
-minutesContainer.innerHTML = ":" + minutes;
-secondsContainer.innerHTML = ":" + seconds;
-*/
-updateClock();
 
+updateClock();
 window.setInterval(updateClock, 1000);
-document.getElementById('smaller').addEventListener("click", function() {
-    fontSize --;
-    document.getElementById('container').style.fontSize = fontSize + "px";
+
+// change between languages
+document.getElementById('et').addEventListener("click", function() {
+    dayContainer.innerHTML = days[day];
+    monthContainer.innerHTML = months[month];
 });
-document.getElementById('bigger').addEventListener("click", function() {
-    fontSize ++;
-    document.getElementById('container').style.fontSize = fontSize + "px";
+document.getElementById('en').addEventListener("click", function() {
+    dayContainer.innerHTML = daysEN[day];
+    monthContainer.innerHTML = monthsEN[month];
+});
+
+// change between 24-hour and 12-hour format
+document.getElementById('time').addEventListener("click", function() {
+    // invert the value of is24Hour
+    is24Hour = !is24Hour;
 });
 
 function updateClock() {
@@ -53,6 +56,26 @@ function updateClock() {
     hours = d.getHours();
     minutes = d.getMinutes();
     seconds = d.getSeconds();
+
+    /*
+    convert 24-hour format to 12-hour format
+    determine am or pm
+    if time is over 12 hours, return the remainder of modulo 12
+    if time is 00, change it to 12
+    */
+    if (!is24Hour) {
+        amPm = " am";
+
+        if (hours >= 12) {
+            amPm = " pm";
+        }
+
+        hours = hours % 12;
+
+        if (hours == 0) {
+            hours = 12;
+        }
+    }
 
     if (hours < 10) {
         hours = "0" + hours;
@@ -66,5 +89,11 @@ function updateClock() {
 
     hoursContainer.innerHTML = hours;
     minutesContainer.innerHTML = ":" + minutes;
-    secondsContainer.innerHTML = ":" + seconds;
+
+    // add or remove am/pm suffix to seconds
+    if (!is24Hour) {
+        secondsContainer.innerHTML = ":" + seconds + amPm;
+    } else {
+        secondsContainer.innerHTML = ":" + seconds;
+    }
 }
